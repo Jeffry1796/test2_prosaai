b7972e13a8ab6f6038df2ccbedb55e2a3166977d
Jeffry1796 <jeffrygsitanaya@gmail.com>
2020-09-06 02:55:49 +0700

    find rtp.payload in Layer RTP, export outfile.raw

--- test2.py
+++ test2.py
@@ -1,6 +1,5 @@
 import os,pyshark
 
-
 class RTP_Audio:
     def __init__ (self,inp_file,out_file,filter):
         self.input = inp_file
@@ -8,13 +7,20 @@ class RTP_Audio:
         self.filter = filter
 
     def to_audio (self):
+        print(self.filter)
+        rtp_list = []
         input_pyshark = pyshark.FileCapture(self.input,display_filter=self.filter)
-        print(input_pyshark)
-        # with open(self.output,'wb') as f:
-        #     f.
-        # res_audio = open(self.output,'wb')
-        #
-        # print(self.input)
+        with open (self.output,'wb') as f:
+            for x in range (100):
+                print(x)
+                rtp_val = input_pyshark[x][3]
+                rtp_list.append(rtp_val.payload.split(':'))
 
+            for x in rtp_list:
+                packet = " ".join(x)
+                res_audio = bytearray.fromhex(packet)
+                f.write(res_audio)
 
-audio = RTP_Audio('input_file.pcap','output','rtp').to_audio()
+        print('DONE')
+
+audio = RTP_Audio('input_file.pcap','output.raw','rtp').to_audio()
