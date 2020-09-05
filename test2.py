f247e74bdac94348e41f8b72f14619811dfe106e
Jeffry1796 <jeffrygsitanaya@gmail.com>
2020-09-06 00:49:47 +0700

    update to_audio method

--- test2.py
+++ test2.py
@@ -1,3 +1,20 @@
-import os
+import os,pyshark
 
-print('TEST')
+
+class RTP_Audio:
+    def __init__ (self,inp_file,out_file,filter):
+        self.input = inp_file
+        self.output = out_file
+        self.filter = filter
+
+    def to_audio (self):
+        input_pyshark = pyshark.FileCapture(self.input,display_filter=self.filter)
+        print(input_pyshark)
+        # with open(self.output,'wb') as f:
+        #     f.
+        # res_audio = open(self.output,'wb')
+        #
+        # print(self.input)
+
+
+audio = RTP_Audio('input_file.pcap','output','rtp').to_audio()
